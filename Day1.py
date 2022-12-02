17ff409f7bb87938629a833f992bea51f0b745bd
slakhani16 <lakhani.shahista@utexas.edu>
2022-12-01 20:55:50 -0600

    Day1

--- Day1.py
+++ Day1.py
@@ -1,1 +1,39 @@
+#Day 1 Puzzle - Return calorie count of Elf that has most snacks 
+
+#Import info of calories each elf is carrying
+
+
+#Parse data into array of strings/int
+
+
+#Assign each elf its calories based on line break
+
+
+#Convert string to int
+
+
+#Total count of calories each elf is carrying
+
+
+#Return total of elf with most calories
+
+with open("data.txt", "r") as f:
+    raw_data = f.read()
+
+    raw_data_list = raw_data.split("\n")
+
+
+record_calories = 0
+current_calories = 0
+
+for item in raw_data_list:
+    if item == "":
+        current_calories = 0
+    else:
+        current_calories += int(item)
+
+    if current_calories > record_calories:
+        record_calories = current_calories
+
+print(f"Record = {record_calories}")
 
